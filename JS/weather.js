da4be497c400d18b11b24510d983d7a50af31ae6
YunBom <ahybi986@naver.com>
2022-08-12 11:19:54 +0900

    최종본

--- JS/weather.js
+++ JS/weather.js
@@ -1,13 +1,20 @@
-const API_KEY = "40c05b28bdc72f6d81";
+// https://openweathermap.org/api - 날씨 API
+
+
+const API_KEY = "40c05b28bdc72f6d8159841f66b71074";
 function onGeoOk(position){     // position : JS가 user의 postition을 전달하는 매개변수.
-    console.log(position);
+    console.log(position);  // onGeoOK가 받는 매개변수 object가 무엇인지 보여줌 [ GeolocationPosition ]
     const lat = position.coords.latitude;   // latitude 경도
-    const lng = position.coords.longitude;   // longitude 위도
-    console.log("You live in", lat, lng);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+    const lon = position.coords.longitude;   // longitude 위도
+    console.log("You live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+        // 화씨에서 섭씨온도를 얻기 위해 url 뒤에 [ &units=metric ]을 추가
     fetch(url).then(Response => Response.json()).then(data => {
-        console.log(data.name, data.weather[0].main);
-    });     // JS에서 url 호출. 
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `날씨 ${data.weather[0].main} / 온도 ${data.main.temp}`;
+    });     // JS에서 url 호출. F12 네트워크
 };
 function onGeoError(){
     alert("Can't find you. No weather for you.");
